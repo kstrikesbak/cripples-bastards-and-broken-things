883b9a5e4f84f3133cba09df2ea082a1b4cb88da
Kejal Bhuva <Kejal.Bhuva@codeimmersives.com>
2019-10-26 18:38:18 -0400

    complete

--- you-deserve-arrays.js
+++ you-deserve-arrays.js
@@ -1,25 +1,27 @@
 function getFirstItemFrom(arr) {
-  return arr[1];
+  return arr[0]; 
 }
 
 function getLastItemFrom(arr) {
-  return arr.length - 1;
+  return arr[arr.length - 1];
 }
 
 function getIndex3(arr) {
-  if (arr.length < 4) {
-    return getLastItemFrom(arr);
-  }
+  // if (arr.length > 3) {
+  //   return arr[3];
+  // }
 
-  return arr[4];  
+  // return arr[arr.length-1];  
+  return arr.length > 3 ? arr[3] : arr[arr.length-1]
 }
 
 function firstItemIsNumber(arr) {
-  return getFirstItemFrom(arr) === 'number'
+  return typeof arr[0] === 'number'
 }
 
 function isLongList(arr) {
-  return arr.length >= 10 && arr.length < 20;
+  // return arr.length >= 10 && arr.length < 20;
+  return arr.length > 9 
 }
 
 
